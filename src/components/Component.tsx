68efbbd7cec305ae955b66ad63aca007f59ca40d
Vanessa Rodrigues <vanessarodrigues010506@gmail.com>
2024-06-21 12:41:28 +0530

    cards done

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,17 +1,21 @@
 'use client'
 import { useState } from 'react'
+import { EvervaultCard, Icon } from './ui/evervault-card'
 
 export default function MyComponent() {
-  const [state, setState] = useState('')
-
-  const handleClick = () => {
-    setState('Button clicked')
-  }
-
   return (
     <div>
-      <p>State: {state}</p>
-      <button onClick={handleClick}>Click me</button>
+      <div className="items start > * px-space-x-6 > * m-8 flex max-w-min flex-row space-x-2">
+        <div className="relative m-2 mx-auto flex h-[30rem] max-w-sm flex-col items-start p-4 px-1 ">
+          <EvervaultCard imgsrc="/gdsc-f.png" />
+        </div>
+        <div className="relative m-2 mx-auto flex h-[30rem] max-w-sm flex-col items-start p-4 px-1">
+          <EvervaultCard imgsrc="/gdsc-f.png" />
+        </div>
+        <div className="relative m-2 mx-auto flex h-[30rem] max-w-sm flex-col items-start p-4 px-1">
+          <EvervaultCard imgsrc="/gdsc-f.png" />
+        </div>
+      </div>
     </div>
   )
 }
