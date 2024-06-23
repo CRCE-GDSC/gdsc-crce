091877e29fb851c91fbfe2cb08b6c8db0a5d5b8c
arshdeepkaur22 <arshd33p.kaur05@gmail.com>
2024-06-23 22:04:50 +0530

    meteors effect

---
+++ src/components/Aboutus.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { cn } from '@/lib/utils'
+import { Meteors } from './Meteors'
+
+const Aboutus = () => {
+  return (
+    <div>
+      <div
+        className="container mx-auto scroll-mt-96 p-4 max-sm:mt-0 xl:py-16"
+        id="about"
+      >
+        <div className="flex justify-center">
+          <h3 className="font- x-shadow-yellow text-white-200 mb-6 w-auto items-center justify-center px-4 py-3 text-center text-5xl max-lg:text-4xl max-md:text-4xl max-sm:mt-0 max-sm:text-3xl">
+            About us
+          </h3>
+        </div>
+
+        <div className="mt-10 grid grid-cols-1 gap-5 max-sm:px-2 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+          <div className="feature-card d-shadow-blue overflow-hidden border p-5 hover:bg-gray-900">
+            <div className="mx-4 mb-4 flex h-10 w-10 items-center justify-center rounded-full">
+              <svg
+                className="h-8 w-8 text-xl text-blue-500"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                ></path>
+              </svg>
+            </div>
+            <div className="font-poppins mb-3 text-xl font-bold">
+              <h4 className="font-poppins mx-4 text-gray-500">
+                Concept of GDSC
+              </h4>
+            </div>
+            <p className="font-poppins mx-4 pb-5 text-base text-gray-700">
+              The GDSC program is a grassroots channel through which Google
+              provides development, mobile &amp; web development skills for
+              students, towards employability.
+            </p>
+          </div>
+          <div className="d-shadow-green feature-card overflow-hidden p-5">
+            <div className="mx-4 mb-4 flex h-10 w-10 items-center justify-center rounded-full">
+              <svg
+                className="h-8 w-8 text-green-500"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M22 12h-4l-3 9L9 3l-3 9H2"
+                ></path>
+              </svg>
+            </div>
+            <div className="font-poppins mb-2 text-xl font-bold">
+              <h4 className="mx-4 text-gray-500">Why GDSC?</h4>
+            </div>
+            <p className="font-poppins mx-4 pb-5 text-base text-gray-700">
+              For students to learn development skills, solve problems through
+              technology and inspire them to be world className developers and
+              change makers.
+            </p>
+          </div>
+          <div className="d-shadow-red feature-card overflow-hidden p-5">
+            <div className="mx-4 mb-4 flex h-10 w-10 items-center justify-center rounded-full">
+              <svg
+                className="h-8 w-8 text-red-500"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 10V3L4 14h7v7l9-11h-7z"
+                ></path>
+              </svg>
+            </div>
+            <div className="mb-2 text-xl font-bold">
+              <h4 className="mx-4 text-gray-500">Target audience</h4>
+            </div>
+            <p className="mx-4 text-base text-gray-700">
+              GDSC activities are targeted at University students and any others
+              including faculty members who want to learn development skills
+              &amp; work to solve real-life problems.
+            </p>
+          </div>
+        </div>
+        <Meteors number={20} />
+      </div>
+    </div>
+  )
+}
+
+export default Aboutus
