81a1ca60562360304565d1eb8b84645bfaa03dc8
arshdeepkaur22 <arshd33p.kaur05@gmail.com>
2024-06-23 22:09:17 +0530

    meteors effect2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,19 @@ export default {
   // darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        'meteor-effect': 'meteor 5s linear infinite',
+      },
+      keyframes: {
+        meteor: {
+          '0%': { transform: 'rotate(215deg) translateX(0)', opacity: '1' },
+          '70%': { opacity: '1' },
+          '100%': {
+            transform: 'rotate(215deg) translateX(-500px)',
+            opacity: '0',
+          },
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
