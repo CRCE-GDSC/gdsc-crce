f92d2e8c8ef3dc75675d5218ae84ae4e58a8d6a8
shariandabre <dabresharian@gmail.com>
2024-07-04 22:55:12 +0530

    graymap

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -2,14 +2,13 @@
 import React from 'react'
 import { StackedCarousel } from 'react-stacked-carousel'
 import 'react-stacked-carousel/dist/index.css'
-import { ArrowRightIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { AceGridCard } from './ui/ace-grid'
 
 import Image from 'next/image'
 
 export default function StackedCards(): JSX.Element {
   return (
-    <div className="">
+    <div className="mb-10">
       <div className="flex items-center justify-center">
         <Image
           src="/assets/gdsc_logo_left.png"
@@ -27,20 +26,24 @@ export default function StackedCards(): JSX.Element {
           className="moveRight z-10 -translate-x-[100px]"
         />
       </div>
-      <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex items-center justify-center">
         <StackedCarousel
           autoRotate={false}
           showSummary={false}
-          cardClassName="w-[400px] h-[600px] bg-neutral-900 rounded-xl shadow-lg p-4 m-4"
-          containerClassName="w-[400px] h-[600px] shadow-lg px-10 mr-10 ml-3 "
+          cardClassName="w-auto h-96 bg-neutral-900 rounded-xl shadow-lg p-4 m-4"
+          containerClassName="w-96 h-96 shadow-lg px-10 mr-10 ml-3 "
           leftButton={
             <button className="rounded-lg bg-black p-2 text-white">
-              <img src="/gdsc-fl.png" height={25} width={25} alt="left" />
+              <img src="/gdsc-fl.png" height={15} width={18} alt="" />
             </button>
           }
           rightButton={
             <button className="rounded-lg bg-black p-2 text-white">
+<<<<<<< Updated upstream
               <img src="/gdsc-fr.png" height={15} width={25} alt="" />
+=======
+              <img src="/gdsc-fr.png" height={15} width={18} alt="" />
+>>>>>>> Stashed changes
             </button>
           }
         >
@@ -48,75 +51,59 @@ export default function StackedCards(): JSX.Element {
             key="Image1"
             src="/mathday.jpeg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image2"
             src="/mathday.jpeg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image3"
             src="/mathday.jpeg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image4"
             src="/mathday.jpeg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image5"
             src="/postman_event_poster.webp"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image6"
             src="/ideation.jpg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image7"
             src="/Bit-N-Build.jpg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
           <Image
             key="Image8"
             src="/HackOver3.0.jpg"
             alt="events"
-            height={500}
-            width={500}
+            height={200}
+            width={200}
           />
         </StackedCarousel>
-      </div>
-      <div className="mx-[45%] p-4">
-        <Button
-          variant="ghost"
-          asChild
-          size="2"
-          className="pointer-events-auto flex w-fit items-center rounded-2xl border border-neutral-600 p-2 transition-all duration-300 ease-in-out hover:-translate-y-3 hover:bg-neutral-800 hover:shadow-md"
-          onClick={() => {
-            window.open('/allevents')
-          }}
-        >
-          <p className="flex items-center">
-            View All Events
-            <ArrowRightIcon className="ml-2 h-6 w-7" />
-          </p>
-        </Button>
       </div>
     </div>
   )
--- src/components/footerr.tsx
+++ src/components/footerr.tsx
@@ -95,12 +95,13 @@ const Footerr = () => {
         <div className="map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.4410136005313!2d72.81818181433687!3d19.04433805791724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c9410830616d%3A0x111b63353dbbce01!2sFr.%20Conceicao%20Rodrigues%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1660900113753!5m2!1sen!2sin"
+            style={{ filter: "grayscale(1) contrast(0.9) opacity(0.7)" }}
             width="100%"
             height="200px"
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-            className="mb-5 mt-10 flex items-start justify-start rounded-3xl"
-          ></iframe>
+            className="mb-5 mt-10 flex items-start justify-start  rounded-3xl"
+          />
         </div>
       </div>
     </div>
