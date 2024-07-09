7e20cc98f71883d7b7f27f4369b429ba607acc5e
ZaneFerns360 <zane.ferns360@gmail.com>
2024-07-09 11:19:25 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use server'
 import React from 'react'
 import { CanvasRevealEffectDemo } from '@/components/aboutuss'
-import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline'
 import WhatWeDo from '@/components/WhatWeDo'
 import Carousel from '@/components/final_c'
 import Hero from '@/components/Hero'
@@ -12,13 +12,17 @@ export default async function Home() {
   await delay(1000) // Add a delay of 1 second (adjust as needed)
   return (
     <div className="flex min-h-screen w-fit flex-col items-center justify-center">
-      <Spline className='h-screen' scene="/assets/gdsc.splinecode" />
+      <div className="h-screen w-full items-center justify-center">
+        <Spline
+          scene="/assets/gdsc.splinecode"
+          className="min-h-screen w-full"
+        />
+      </div>
       <main className="flex-grow">
         {/* <Hero /> */}
         <CanvasRevealEffectDemo />
         <WhatWeDo />
         <Carousel />
-        
       </main>
     </div>
   )
