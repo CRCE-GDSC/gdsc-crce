e9ca2bcd622748dfd34394c0580a70b7bbf41d70
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-07-09 22:28:05 +0530

    team fucksht

---
+++ src/components/Team2.tsx
@@ -0,0 +1,97 @@
+/* eslint-disable react/jsx-key */
+'use client'
+
+import Link from 'next/link'
+import { BackgroundGradient } from './ui/background-gradient'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import DotPattern from './ui/dot-pattern'
+
+export default function TeamComponent() {
+  let data = [
+    {
+      img: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Team 2024',
+      description:
+        'Lorem when an unknown printer took a galley of type and scrambled it to make a type specimen book. onic typesetting, remaining essentially unchanged.',
+    },
+    {
+      img: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Team 2023',
+      description:
+        'Lorem when an unknown printer took a galley of type and scrambled it to make a type specimen book. onic typesetting, remaining essentially unchanged.',
+    },
+    {
+      img: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600',
+      title: 'Team 2022',
+      description:
+        'Lorem when an unknown printer took a galley of type and scrambled it to make a type specimen book. onic typesetting, remaining essentially unchanged.',
+    },
+  ]
+
+  return (
+    <div className="h-full w-full flex-1">
+      <VerticalTimeline lineColor="rgba(100,100,100,0.5)">
+        {data.map((item, idex) => (
+          <VerticalTimelineElement
+            visible={true}
+            className="vertical-timeline-element--work"
+            contentStyle={{
+              background: 'transparent',
+              boxShadow: '0px 0px 20px -2px rgba(0, 0, 0, 0.16)',
+            }}
+            contentArrowStyle={{ borderRight: '7px solid  #4285F4' }}
+            dateClassName="date"
+            date="2023 - present"
+            iconStyle={{ background: 'rgb(19, 19, 19)', color: '#fff' }}
+            icon={<img src="./gdsc-f.png" />}
+          >
+            {' '}
+            <BackgroundGradient key={idex}>
+              <div
+                key={idex}
+                className="max-w-md rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-zinc-900"
+              >
+                <img className="rounded-t-lg" src={item.img} alt="" />
+
+                <div className="p-5">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {item.title}
+                  </h5>
+
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {item.description}
+                  </p>
+                  <Link
+                    href={`/team/${item.title}`}
+                    className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    View Team
+                    <svg
+                      className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                      />
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </BackgroundGradient>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </div>
+  )
+}
