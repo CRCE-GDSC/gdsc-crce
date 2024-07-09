010841e08d0d3e90db4e0eda6ebfb605ced04e9c
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-07-10 01:12:27 +0530

    event button

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -18,12 +18,12 @@ function ResponsiveCarousel() {
         </div>
       </div>
 
-      {/* <button
+       <button
         className="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
         onClick={() => (window.location.href = '/allevents')}
       >
         Go to All Events
-      </button> */}
+      </button> 
     </div>
   )
 }
