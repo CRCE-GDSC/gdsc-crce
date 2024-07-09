cf5072332607021dfe2fe3ce576cada1eb3bb8ba
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-07-09 22:28:39 +0530

    team 1

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,37 +1,56 @@
-/* eslint-disable react/jsx-key */
-'use client'
 
-import Link from 'next/link'
-import { BackgroundGradient } from './ui/background-gradient'
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css'
-import DotPattern from './ui/dot-pattern'
+"use client";
+import Image from 'next/image'
 
-export default function TeamComponent() {
-  let data = [
-    {
-      img: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Team 2024',
-      description:
-        'Lorem when an unknown printer took a galley of type and scrambled it to make a type specimen book. onic typesetting, remaining essentially unchanged.',
-    },
-    {
-      img: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Team 2023',
-      description:
-        'Lorem when an unknown printer took a galley of type and scrambled it to make a type specimen book. onic typesetting, remaining essentially unchanged.',
-    },
-    {
-      img: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600',
-      title: 'Team 2022',
-      description:
-        'Lorem when an unknown printer took a galley of type and scrambled it to make a type specimen book. onic typesetting, remaining essentially unchanged.',
-    },
-  ]
+import React from "react";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+import { ChevronRight } from "lucide-react";
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    designation: "Software Engineer",
+    image:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+  },
+  {
+    id: 2,
+    name: "Robert Johnson",
+    designation: "Product Manager",
+    image:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    name: "Jane Smith",
+    designation: "Data Scientist",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    designation: "UX Designer",
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 5,
+    name: "Tyler Durden",
+    designation: "Soap Developer",
+    image:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+  },
+  {
+    id: 6,
+    name: "Dora",
+    designation: "The Explorer",
+    image:
+      "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+  },
+];
 
+export function AnimatedTooltipPreview() {
   return (
     <div className="h-full w-full flex-1">
       <VerticalTimeline lineColor="rgba(100,100,100,0.5)">
