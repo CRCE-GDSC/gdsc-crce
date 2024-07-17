963667fa6b9ab7e2dcf5546cfa056a45e0620896
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-07-18 00:02:33 +0530

    commit 1 bg fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import ResponsiveCarousel from '@/components/Events'
 import TeamIntro from '@/components/teamIntro/TeamIntro'
 import Hero from '@/components/Hero'
+import DotPattern from '@/components/ui/dot-pattern'
+import { cn } from '@/lib/utils'
 
 //import { HomeTeam } from '@/components/home-team'
 
@@ -11,10 +13,16 @@ export default async function Home() {
     new Promise((resolve) => setTimeout(resolve, ms))
   await delay(1000) // Add a delay of 1 second (adjust as needed)
   return (
-    <div className="flex min-h-screen min-w-screen flex-col items-center justify-center">
-        <Hero />
-        <ResponsiveCarousel />
-        <TeamIntro />
-      </div>
+    <div className="min-w-screen flex min-h-screen flex-col items-center justify-center bg-white  dark:bg-black ">
+      <Hero />
+      <ResponsiveCarousel />
+      <TeamIntro />
+
+      <DotPattern
+        className={cn(
+          '[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)]'
+        )}
+      />
+    </div>
   )
 }
