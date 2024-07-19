a9762a6efd0b9336cbfa38af9f93c47e549304c8
vanessa-rodrigues-156 <vanessarodrigues010506@gmail.com>
2024-07-19 14:10:54 +0530

    comiit 12.1

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -55,11 +55,21 @@ function ResponsiveCarousel() {
   }, [])
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-inherit dark:bg-black" id="events">
+    <div
+      className="flex min-h-screen w-full flex-col items-center justify-center bg-inherit dark:bg-black"
+      id="events"
+    >
+      <div className="mb-20">
+        <h1 className="h2">Events</h1>
+      </div>
       {isMobile ? <VerticalCarousel /> : <Carousel />}
       <button
         className="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        onClick={() => window.open('https://gdsc.community.dev/fr-conceicao-rodrigues-college-of-engineering-mumbai-india/')}
+        onClick={() =>
+          window.open(
+            'https://gdsc.community.dev/fr-conceicao-rodrigues-college-of-engineering-mumbai-india/'
+          )
+        }
       >
         Go to All Events
       </button>
@@ -67,4 +77,4 @@ function ResponsiveCarousel() {
   )
 }
 
-export default ResponsiveCarousel 
+export default ResponsiveCarousel
