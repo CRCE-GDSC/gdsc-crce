6c004605223fce62ed08d08a258cfde4d74ed987
BeatCo <chrislopes3667@gmail.com>
2024-07-19 12:23:15 +0530

    fixes

--- src/components/Aboutus.tsx
+++ src/components/Aboutus.tsx
@@ -22,19 +22,19 @@ const RenderFeature: React.FC<RenderFeatureProps> = ({
 }) => {
   return (
     <div className="feature-item">
-      <div className="feature-item-left" style={{ backgroundColor: color }}>
+      <div className="feature-item-left overflow-visible" style={{ backgroundColor: color }}>
         <Image
           src={image}
           alt={title}
           height={1000}
           width={1000}
           priority
-          className={` mobile min-w-[100px] max-w-[650px] -translate-y-[53.5px] md:-translate-x-[38px]  md:-translate-y-[0px] vn:min-w-[650px] vn:-translate-y-[53.5px] feature-${key}`}
+          className={`min-w-[100px] min-h-full max-w-[650px] overflow-visible -translate-y-[53.5px] md:-translate-x-[38px] md:-translate-y-[0px] vn:min-w-[650px] vn:-translate-y-[53.5px] feature-${key}`}
         />
       </div>
       <div className="feature-item-right">
-        <p className="h2 tanText dark:text-neutral-100">{title}</p>
-        <p className="t2 darkGrey">{description}</p>
+        <p className="h2 tanText z-10 dark:text-neutral-100">{title}</p>
+        <p className="t2 darkGrey z-10">{description}</p>
       </div>
     </div>
   )
@@ -44,32 +44,30 @@ const featuresInfo: FeatureInfo[] = [
   {
     color: '#4285F4',
     image: '/ideate.png',
-    title: 'We ideate',
+    title: 'We Ideate',
     description:
       'We brainstorm for new ideas and chalk up a path to implement them successfully',
   },
   {
     color: '#FBA416',
     image: '/communicate.png',
-    title: 'We communicate',
+    title: 'We Communicate',
     description:
       'We help to hone intelligent minds and develop a dynamic environment through dialogue',
   },
   {
     color: '#594DD2',
     image: '/collaborate.png',
-    title: 'We collaborate',
+    title: 'We Collaborate',
     description: 'We work together on projects and empower the whole community',
   },
 ]
 
 const AboutUs: React.FC = () => {
   return (
-    <div className="w-full ">
-      <p className="h2 darkText ta-center font-bold dark:text-neutral-300">
-        About Us
-      </p>
-      <div className='featurecontainer '>
+    <div className="min-w-full">
+      <p className="h2 darkText ta-center font-bold">About Us</p>
+      <div className="featurecontainer mt-10">
         <div className="features">
           {featuresInfo.map((item, index) => (
             <RenderFeature
