49f55fd16bfcf3ee98ef02f172fdb3c372165f21
chris <chris@localhost.localdomain>
2024-08-25 23:43:31 +0530

    fix

--- src/components/footerr.tsx
+++ src/components/footerr.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 const Footerr = () => {
   return (
     <div className="pt-20 bg-inherit dark:bg-black">
@@ -12,20 +13,20 @@ const Footerr = () => {
             <div className="grid">
               <div className="pb-4 text-gray-900 ease-in-out hover:translate-x-1 hover:text-red-500 hover:underline dark:text-gray-200 dark:hover:text-red-500">
                 {' '}
-                <a href="/">Home </a>
+                <Link href="/#home">Home </Link>
               </div>
               <div className="pb-4 text-gray-900 ease-in-out hover:translate-x-1 hover:text-blue-500 hover:underline dark:text-gray-200 dark:hover:text-blue-500">
                 {' '}
-                <a href="#events">Events</a>
+                <Link href="/#events">Events</Link>
               </div>
               <div className="pb-4 text-gray-900 ease-in-out hover:translate-x-1 hover:text-green-500 hover:underline dark:text-gray-200 dark:hover:text-green-500">
                 {' '}
-                <a href="#team">Our Team</a>
+                <Link href="/#team">Our Team</Link>
               </div>
               <div className="pb-4 text-gray-900 ease-in-out hover:translate-x-1 hover:text-yellow-500 hover:underline dark:text-gray-200 dark:hover:text-yellow-500">
-                <a href="https://developers.google.com/community-guidelines">
+                <Link href="https://developers.google.com/community-guidelines">
                   Community Guidelines
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -41,7 +42,7 @@ const Footerr = () => {
             </div>
             <div className="flex flex-row py-4" id="foot">
               <div className="mr-10 flex h-12 w-12 items-center justify-center rounded-full bg-white bg-opacity-40 hover:bg-blue-300">
-                <a
+                <Link
                   className=""
                   href="https://www.instagram.com/gdsc_crce/"
                   target="_blank"
@@ -55,10 +56,10 @@ const Footerr = () => {
                   >
                     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                   </svg>
-                </a>
+                </Link>
               </div>
               <div className="mr-10 flex h-12 w-12 items-center justify-center rounded-full bg-white bg-opacity-40 hover:bg-blue-300">
-                <a
+                <Link
                   className=""
                   href="https://www.linkedin.com/company/gdsc-crce/mycompany/"
                   target="_blank"
@@ -71,13 +72,13 @@ const Footerr = () => {
                   >
                     <path d="M20.76 0H3.24C1.45 0 0 1.45 0 3.24v17.52C0 22.55 1.45 24 3.24 24h17.52c1.79 0 3.24-1.45 3.24-3.24V3.24C24 1.45 22.55 0 20.76 0zM7.48 19.27H4.67V9.71h2.81v9.56h-.01zM6.08 8.06a2.12 2.12 0 1 1 0-4.24 2.12 2.12 0 0 1 0 4.24zM19.27 19.27h-2.81v-4.99c0-1.19-.02-2.72-1.66-2.72-1.66 0-1.91 1.3-1.91 2.64v5.07h-2.81V9.71h2.7v1.4h.04c.37-.7 1.28-1.44 2.66-1.44 2.84 0 3.36 1.87 3.36 4.3v4.3z" />
                   </svg>
-                </a>
+                </Link>
               </div>
               <div
                 id="contact"
                 className="mr-10 flex h-12 w-12 items-center justify-center rounded-full bg-white bg-opacity-40 hover:bg-blue-300"
               >
-                <a
+                <Link
                   className=""
                   href="https://twitter.com/gdsc_crce"
                   target="_blank"
@@ -93,7 +94,7 @@ const Footerr = () => {
                       d="M 2.3671875 3 L 9.4628906 13.140625 L 2.7402344 21 L 5.3808594 21 L 10.644531 14.830078 L 14.960938 21 L 21.871094 21 L 14.449219 10.375 L 20.740234 3 L 18.140625 3 L 13.271484 8.6875 L 9.2988281 3 L 2.3671875 3 z M 6.2070312 5 L 8.2558594 5 L 18.033203 19 L 16.001953 19 L 6.2070312 5 z"
                     />{' '}
                   </svg>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
